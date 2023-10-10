903afab4c814fcd53a6d8dc47a562fbaf84e01ca
bblith <chloecasuga16@gmail.com>
2023-10-10 16:58:27 -0500

    Commit

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -2,16 +2,20 @@ package com.example.flixster
 
 import com.google.gson.annotations.SerializedName
 
+
 class Movie {
 
     @JvmField
     @SerializedName("title")
     var title: String? = null
 
-    @SerializedName("movie_image")
+    @SerializedName("poster_path")
     var posterURL: String? = null
 
-    @SerializedName("description")
+    @SerializedName("overview")
     var description: String? = null
+
+    //@SerializedName("movie_image")
+    //    var movieImageUrl: String? = null
 
 }
--- app/src/main/java/com/example/flixster/MovieFragment.kt
+++ app/src/main/java/com/example/flixster/MovieFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.RequestParams
@@ -16,6 +17,8 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import okhttp3.Headers
+import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
 private const val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -30,7 +33,7 @@ class MovieFragment : Fragment(), OnListFragmentInteractionListener {
         val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
         val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
         val context = view.context
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = LinearLayoutManager(context)
         updateAdapter(progressBar, recyclerView)
         return view
     }
@@ -43,7 +46,7 @@ class MovieFragment : Fragment(), OnListFragmentInteractionListener {
         params["api-key"] = API_KEY
 
         client[
-            "https://api.themoviedb.org/3/movie/now_playing",
+            "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed",
             params,
             object : JsonHttpResponseHandler() { //connect these callbacks to your API call
 
@@ -55,14 +58,18 @@ class MovieFragment : Fragment(), OnListFragmentInteractionListener {
                 ) {
                     progressBar.hide()
 
-                    val resultsJSON : JSONObject = json.jsonObject.get("results") as JSONObject
-                    val movieRawJSON : String = resultsJSON.get("movies").toString()
+
+
+                    // The wait for a response is over
+                    val resultsJSON : String = json.jsonObject.get("results").toString()
+                    //val moviesRawJSON : String = resultsJSON.get("results").toString()
+
                     val gson = Gson()
                     val arrayMovieType = object : TypeToken<List<Movie>>() {}.type
-                    val models : List<Movie> = gson.fromJson(movieRawJSON, arrayMovieType)
 
+                    val models : List<Movie> = gson.fromJson(resultsJSON, arrayMovieType)
                     recyclerView.adapter = MovieRecyclerViewAdapter(models, this@MovieFragment)
-                    Log.v("MovieFragment", "Response Successful")
+                    Log.d("MovieFragment", "Response Successful")
                 }
 
                 override fun onFailure(
